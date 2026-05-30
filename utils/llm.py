81fef1a42b1c0c5edc4abb5d6b686a2a630d5a42
vedant bhoyar <vedantbhoyarofficial@gmail.com>
2026-05-31 01:26:59 +0530

    Update llm.py

--- utils/llm.py
+++ utils/llm.py
@@ -1,23 +1,16 @@
 import os
 from groq import Groq
-import streamlit as st
-from groq import Groq
 
-client = Groq(api_key=st.secrets["GROQ_API_KEY"])
-client = Groq(api_key=")
+client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
 
-def call_llm(model, messages, system="", max_tokens=8192, retries=3, retry_delay=2.0):
+def call_llm(model, messages, system="", max_tokens=4096, retries=3, retry_delay=2.0):
     all_messages = []
-    
     if system:
         all_messages.append({"role": "system", "content": system})
-    
     all_messages.extend(messages)
-    
     response = client.chat.completions.create(
         model="llama-3.3-70b-versatile",
         messages=all_messages,
         max_tokens=max_tokens
     )
-    
     return response.choices[0].message.content
